41b99fc3f1cc1b79b88aa0b9ca16ec230505f050
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2018-10-18 02:23:22 -0300

    Correção de rota ao clicar no logo do sistema

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,7 @@
                 <!-- Logo -->
                 <!-- ============================================================== -->
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="index.html">
+                    <a class="navbar-brand" href="{{ route('dashboard') }}">
                         <!-- Logo icon -->
                         <b>
                             <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
